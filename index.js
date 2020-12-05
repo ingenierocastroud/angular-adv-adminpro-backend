8a34ea6ae30c5fda1cdc08bde574160a21e4ae7e
Manuel Castro <ingenierocastroud@gmail.com>
2020-12-05 13:44:30 -0500

    ajuste despliegue manejo ruta angular

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require('express');
 require('dotenv').config();
 const {dbConnection}=require('./db/config')
 const cors=require('cors')
+const path =require('path')
 
 ///crear el servidor express
 const app = express();
@@ -25,6 +26,11 @@ app.use('/api/medicos',require('./routes/medicos'));
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/todo',require('./routes/busquedas'));
 app.use('/api/uploads',require('./routes/uploads'));
+
+//CAMBIO APP DESPLEGADO JUNTO AL BACKEND
+app.get('*',(req,resp)=>{
+        resp.sendFile(path.resolve(__dirname,'public/index.html'))
+})
 
 
 app.listen(process.env.PORT,()=>{
